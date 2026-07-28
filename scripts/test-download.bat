95a4c1c0e0cb9786259bb871df3c7ce0586f4071
q5n <q5n@gmail.com>
2026-07-28 15:52:36 +0800

    modify test-download.bat

--- scripts/test-download.bat
+++ scripts/test-download.bat
@@ -3,6 +3,10 @@ set "DlSecret=your-secret"
 set "DlProxy=https://your-domain.com/downloadpath"
 set "DlTarget=https://github.com/{USER}/{REPO}/releases/download/{TAG}/{FILE}"
 
-powershell -NoProfile -c "&{param($u)$t=[DateTimeOffset]::UtcNow.ToUnixTimeSeconds();$L='%DlProxy%?url='+[Net.WebUtility]::UrlEncode($u)+'&time='+$t+'&sign='+([BitConverter]::ToString([Security.Cryptography.HMACSHA256]::new([Text.Encoding]::UTF8.GetBytes('%DlSecret%')).ComputeHash([Text.Encoding]::UTF8.GetBytes('url='+$u+'&time='+$t))) -replace '-','').ToLower();iwr $L -OutFile ([uri]$u).Segments[-1]} '%DlTarget%'"
+@rem Invoke-WebRequest version
+@rem powershell -NoProfile -c "&{param($u)$t=[DateTimeOffset]::UtcNow.ToUnixTimeSeconds();$c=[Text.Encoding]::UTF8;$L='%DlProxy%?url='+[Net.WebUtility]::UrlEncode($u)+'&time='+$t+'&sign='+([BitConverter]::ToString([Security.Cryptography.HMACSHA256]::new($c.GetBytes('%DlSecret%')).ComputeHash($c.GetBytes('url='+$u+'&time='+$t))) -replace '-','').ToLower();iwr $L -OutFile ([uri]$u).Segments[-1]} '%DlTarget%'"
+
+@rem curl version , faster
+powershell -NoProfile -c "&{param($u)$t=[DateTimeOffset]::UtcNow.ToUnixTimeSeconds();$c=[Text.Encoding]::UTF8;$L='%DlProxy%?url='+[Net.WebUtility]::UrlEncode($u)+'&time='+$t+'&sign='+([BitConverter]::ToString([Security.Cryptography.HMACSHA256]::new($c.GetBytes('%DlSecret%')).ComputeHash($c.GetBytes('url='+$u+'&time='+$t))) -replace '-','').ToLower();curl.exe $L -o ([uri]$u).Segments[-1]} '%DlTarget%'"
 
 pause
